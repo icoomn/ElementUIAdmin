526080c403173bf13502540fe8c757ec318e3687
yunfeng <281542025@qq.com>
2019-01-10 21:35:19 +0800

    modify login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="login-box">
-    <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" class="login-form">
+    <el-form :model="loginForm" :rules="rules" ref="loginForm" class="login-form">
       <h3 class="title">系 统 登 录</h3>
       <el-form-item prop="username">
-        <el-input v-model="loginForm.username" autocomplete="off" placeholder="用户名: admin"></el-input>
+        <el-input
+          v-model="loginForm.username"
+          autocomplete="off"
+          placeholder="账 号: admin"
+          prefix-icon="el-icon-goods">
+        </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input type="password" v-model="loginForm.password" @keyup.native.enter="login('loginForm')" autocomplete="off" placeholder="密 码: admin"></el-input>
+        <el-input :type="textType"
+          v-model="loginForm.password"
+          @keyup.native.enter="login('loginForm')"
+          autocomplete="off"
+          placeholder="密 码: admin"
+          prefix-icon="el-icon-time">
+          <i slot="suffix" class="el-input__icon el-icon-view btn-eye" @click="changeType"></i>
+        </el-input>
       </el-form-item>
       <el-form-item v-show="showMsg" style="margin-bottom:0;">
         <span class="text-danger">提示：用户名或密码错误，请重试！</span>
@@ -35,7 +47,8 @@ export default {
         ]
       },
       loading: false,
-      showMsg: false
+      showMsg: false,
+      textType: 'password'
     }
   },
   methods: {
@@ -61,6 +74,9 @@ export default {
           return false
         }
       })
+    },
+    changeType () {
+      this.textType = (this.textType === 'password' ? 'text' : 'password')
     }
   }
 }
@@ -85,6 +101,9 @@ export default {
       margin: 0px auto 40px auto;
       text-align: center;
       color: #505458;
+    }
+    .btn-eye {
+      cursor: pointer;
     }
     .text-danger {
       color: #F56C6C;
